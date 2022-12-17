5c6cda2fada3b309e69b65b8da2db3617dbd82ac
Black-Male <Gkaranja20@gmail.com>
2022-12-17 11:52:12 +0300

    Basic Search abilities implemented

--- java/index.js
+++ java/index.js
@@ -1,8 +1,14 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("")
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
+    .then(res => res.json())
+    .then(data => console.log(data))
 
     document.querySelector('.search-inputs-container').addEventListener('submit', (e) => {
         e.preventDefault()
-        
+        let searchResult = document.querySelector('.search-bar').innerHTML
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchResult}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
     })
 })
