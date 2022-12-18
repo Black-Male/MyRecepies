92981ad4ab46dc89b6d24c43123d2849ba52bd7c
Black-Male <Gkaranja20@gmail.com>
2022-12-18 18:02:03 +0300

    Basic Search Functionality Implemented

--- java/index.js
+++ java/index.js
@@ -1,14 +1,44 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
-    .then(res => res.json())
-    .then(data => console.log(data))
+    // fetch(`https:www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
+    // .then(res => res.json())
+    // .then(data => {
+    //     console.log(data)
+    // })
 
     document.querySelector('.search-inputs-container').addEventListener('submit', (e) => {
         e.preventDefault()
-        let searchResult = document.querySelector('.search-bar').innerHTML
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchResult}`)
+        let inputs = e.target.querySelector('.search-bar').value
+        fetch(`https:www.themealdb.com/api/json/v1/1/filter.php?i=${inputs}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data =>{
+            console.log(data.meals)
+            if (data.meals){
+                data.meals.forEach(meal => {
+                    console.log(meal)
+                })
+            }
+        })
+        
+
     })
 })
+
+function starterPageDisplay(z){
+    let holder = document.createElement('div')
+    holder.id = 'RemoveMe'
+    holder.innerHTML = `
+    <div class="meal-item" id="${z.id}}">
+        <div class="meal-img">
+            <img src="${z.strMeal}" alt="food">        
+        </div>
+        <div class="meal-name">
+            <h3>${z.strMealThumb}</h3>
+            <button class="recipe-btn">Recipe</button>
+        </div>
+    </div>
+    `
+    let AddingPoint = document.querySelector('.food')
+    AddingPoint.appendChild(holder)
+}
+
