553c9e5354b79f1fce315b0ace14d324a8b1b67e
Black-Male <Gkaranja20@gmail.com>
2022-12-19 04:15:33 +0300

    Dummy login start point Created With recepie button as well

--- java/index.js
+++ java/index.js
@@ -1,5 +1,5 @@
 //let holder = ""
-
+let AddingPoint = document.querySelector('.food')
 document.addEventListener('DOMContentLoaded', () => {
     // fetch(`https:www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
     // .then(res => res.json())
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //     console.log(data)
     // })
 
+
+    // search for food by name 
     document.querySelector('.search-inputs-container').addEventListener('submit', (e) => {
         e.preventDefault()
         let inputs = e.target.querySelector('.search-bar').value
@@ -18,23 +20,28 @@ document.addEventListener('DOMContentLoaded', () => {
             if (data.meals){
                 data.meals.forEach(z => {
                     console.log(z)
+                    let id = z.id
                     //mealPageDisplay(meal)
                     holder += `
-                        <div class="meal-item" id="${z.id}}">
+                        <div class="meal-item" id="${z.idMeal}">
                             <div class="meal-img">
                                 <img src="${z.strMealThumb}" alt="food">        
                             </div>
                             <div class="meal-name">
                                 <h3>${z.strMeal}</h3>
-                                <button class="recipe-btn">Recipe</button>
+                                <button class="recipe-btn" id="${z.idMeal}">Recipe</button>
                             </div>
                         </div>
                         `
-                        let AddingPoint = document.querySelector('.food')
                         AddingPoint.innerHTML = holder
-                        holder.querySelector('.recipe-btn').addEventListener('click',() => {
-                            
-                        })
+
+                        //attempt at a show food recipe feature
+                        //AddingPoint.querySelector('.recipe-btn').addEventListener('click',() => {
+                        //   if (z.id === AddingPoint.querySelector('.recipe-btn').id){
+                        //     console.log (z.strInstructions)
+                        //   }  
+                        //console.log()
+                        //})
                         
                 })
             }else{
@@ -45,25 +52,15 @@ document.addEventListener('DOMContentLoaded', () => {
                let AddingPoint = document.querySelector('.food')
                AddingPoint.innerHTML = holder
             }
-        })
-        
+        })       
 
     })
+
+    // dummy login point
+    // document.querySelector('.Login').addEventListener('click', () => {
+
+    // })
 })
+// document.querySelector('.food').addEventListener('click', (y) => {
 
-// function mealPageDisplay(z){
-//     let holder += `
-//     <div class="meal-item" id="${z.id}}">
-//         <div class="meal-img">
-//             <img src="${z.strMealThumb}" alt="food">        
-//         </div>
-//         <div class="meal-name">
-//             <h3>${z.strMeal}</h3>
-//             <button class="recipe-btn">Recipe</button>
-//         </div>
-//     </div>
-//     `
-//     let AddingPoint = document.querySelector('.food')
-//     AddingPoint.innerHTML = holder
-// }
-
+// })
